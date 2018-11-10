b764f9c256905233a4609f3650a974bed07679e5
Philipp <philippe15@live.com>
2018-11-09 20:50:00 -0500

    Include vueitfy plugin & setting turn on auto semicolon on format

--- pafia-2/.eslintrc.js
+++ pafia-2/.eslintrc.js
@@ -10,7 +10,12 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'line-ending': 0
+    'line-ending': 0,
+    // don't require .vue extension when importing
+    'import/extensions': ['off', 'always', {
+      'js': 'never',
+      'vue': 'never'
+    }],
   },
   parserOptions: {
     parser: 'babel-eslint',
